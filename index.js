d6e751876c13350c2ac8ad46fe63524c42e8a78d
Eric Klimowich <eklimowich@hotmail.com>
2018-08-10 03:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var musiciansAndInstruments = [];
-  for (i = 0; i < musicians.length; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     musiciansAndInstruments.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return musiciansAndInstruments;
